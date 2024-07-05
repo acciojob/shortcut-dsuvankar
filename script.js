4171cfba26e92d085211ff2c4f15a0ebaedaeb19
dsuvankar <98757134+dsuvankar@users.noreply.github.com>
2024-07-05 22:21:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function shortcut(s1, s2) {
-  // your code here
+  const arr1 = s1.split('')
+  const arr2 = s2.split('')
+
+if(arr1.length>0 && arr2.length>0){
+	return `${arr1[0]}${arr2[0]}`
+}else{
+	return ''
 }
+} 
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
